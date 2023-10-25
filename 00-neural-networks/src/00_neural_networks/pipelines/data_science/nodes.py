5ec17cf62fb1f9e66d2b29b2ea4828e56a0e5a4b
Juan <jjimeneza@unal.edu.co>
2023-10-24 19:29:14 -0500

    I finished the Network code and did some test with the code in a new notebbook

--- 00-neural-networks/src/00_neural_networks/pipelines/data_science/nodes.py
+++ 00-neural-networks/src/00_neural_networks/pipelines/data_science/nodes.py
@@ -2,6 +2,13 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.7
 """
-from .utilities.Cost_Functions import QuadraticCost, CrossEntropyCost
+import logging
+from typing import Dict, Tuple
 
+from .utilities import Network
+
+
+def train_model(model_input_table: Tuple,parameter: Dict) -> Dict:
+    training_data_list, validation_data_list, test_data_list = model_input_table
+    
 
