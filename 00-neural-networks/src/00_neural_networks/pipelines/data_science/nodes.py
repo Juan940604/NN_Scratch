41f85cb45a69d86df9c46cc08f6e752cd074658e
Juan <jjimeneza@unal.edu.co>
2023-10-25 14:28:43 -0500

    I added the nodes.py and pipelines.py of datascience pipeline

--- 00-neural-networks/src/00_neural_networks/pipelines/data_science/nodes.py
+++ 00-neural-networks/src/00_neural_networks/pipelines/data_science/nodes.py
@@ -4,11 +4,42 @@ generated using Kedro 0.18.7
 """
 import logging
 from typing import Dict, Tuple
+#from .utilities.Network import Network
+from .utilities import  Network
 
-from .utilities import Network
+def train_model(model_input_table: Tuple,params: Dict) -> Dict:
+    training_data, validation_data, test_data = model_input_table
 
+    # Hyperparameters
+    # Layers and neurons
+    sizes = params['sizes']
+    # cost function
+    cost = params['cost']
+    if hasattr(Network, cost):
+        cost = getattr(Network, cost)
+    else: 
+        print(f"Network has no function named {cost}")
+    # Learning rate
+    eta = params['eta']
+    epochs = params['epochs']
+    mini_batch_size = params['mini_batch_size']
+    monitor_evaluation_accuracy = params['monitor_evaluation_accuracy']
+    regularization = params['regularization']
 
-def train_model(model_input_table: Tuple,parameter: Dict) -> Dict:
-    training_data_list, validation_data_list, test_data_list = model_input_table
+    # Neural Network
+    net = Network.Network(sizes=sizes,cost = cost)
+
+    #
+    logger = logging.getLogger(__name__)
+    net.SGD(training_data=training_data,epochs= epochs, mini_batch_size= mini_batch_size, eta= eta,evaluation_data=test_data, monitor_evaluation_accuracy=monitor_evaluation_accuracy,regularization=regularization)
+
+    data = {
+                "sizes" : net.sizes,
+                "weights" : [w.tolist() for w in net.weights],
+                "biases" : [b.tolist() for b in net.biases],
+                "cost": str(net.cost.__name__)
+             }
+
+    return data
     
 
--- 00-neural-networks/src/00_neural_networks/pipelines/data_science/pipeline.py
+++ 00-neural-networks/src/00_neural_networks/pipelines/data_science/pipeline.py
@@ -4,11 +4,16 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import greet
+from .nodes import train_model
+
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-
-
+        node(
+            func=train_model,
+            inputs=["model_input_table",'params:model_options'],
+            outputs='nn_trained',
+            name='train_neural_network',
+            ),
     ])
